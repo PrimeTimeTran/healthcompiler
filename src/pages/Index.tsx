d5edcccf5346591bd32f3d8e18762bea84a69a83
PrimeTimeTran <primetimetran@gmail.com>
2026-01-09 17:09:12 +0700

    Add suspense

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,22 +1,25 @@
-import { Layout } from "@/components/layout/Layout";
-import { HeroSectionAlt } from "@/components/home/HeroSectionAlt";
-import { WhatWeDoSection } from "@/components/home/WhatWeDoSection";
-import { HowWeHelpSection } from "@/components/home/HowWeHelpSection";
-import { IndustriesSection } from "@/components/home/IndustriesSection";
-import { TestimonialsSection } from "@/components/home/TestimonialsSection";
-import { CTASection } from "@/components/home/CTASection";
+import { Suspense } from 'react'
+import { Layout } from '@/components/layout/Layout'
+import { HeroSectionAlt } from '@/components/home/HeroSectionAlt'
+import { WhatWeDoSection } from '@/components/home/WhatWeDoSection'
+import { HowWeHelpSection } from '@/components/home/HowWeHelpSection'
+import { IndustriesSection } from '@/components/home/IndustriesSection'
+import { TestimonialsSection } from '@/components/home/TestimonialsSection'
+import { CTASection } from '@/components/home/CTASection'
 
 const Index = () => {
   return (
     <Layout>
-      <HeroSectionAlt />
-      <WhatWeDoSection />
-      <HowWeHelpSection />
-      <IndustriesSection />
-      <CTASection />
-      <TestimonialsSection />
+      <Suspense fallback={<div>Loading...</div>}>
+        <HeroSectionAlt />
+        <WhatWeDoSection />
+        <HowWeHelpSection />
+        <IndustriesSection />
+        <CTASection />
+        <TestimonialsSection />
+      </Suspense>
     </Layout>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
